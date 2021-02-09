8c180d5f5c54fb02a00ee159b903542c3c82b2d1
Or Kandabi <MJFC37@motorolasolutions.com>
2021-02-09 13:56:00 +0200

    add a function

--- src/Trimmer.js
+++ src/Trimmer.js
@@ -3,14 +3,18 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-ffmpeg('C:/Users/mjfc37/Documents/Motorola/VideoProccess/VideoProccessing/assets/sample1.mp4')
-  .setStartTime('00:01:00')
-  .setDuration('10')
-  .output('C:/Users/mjfc37/Documents/Motorola/VideoProccess/VideoProccessing/assets/sample1out2.mp4')
-  .on('end', function(err) {
-    if(!err) { console.log('conversion Done') }
-  })
-  .on('error', function(err){
-    console.log('error: ', err)
-  })
-  .run()
+const trimVideo=(path,name,newName,startTime,endTime)=>{
+    ffmpeg(path+name+'.mp4')
+    .setStartTime(startTime)
+    .setDuration(endTime)
+    .output(path+name+newName+'.mp4')
+    .on('end', function(err) {
+        if(!err) { console.log('conversion Done') }
+    })
+    .on('error', function(err){
+        console.log('error: ', err)
+    })
+    .run()
+}
+
+trimVideo('C:/Users/mjfc37/Documents/Motorola/VideoProccess/VideoProccessing/assets/','sample1','sample1out3',100,10);
